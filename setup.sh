a9c056a15b68efa23f8f5d1704775f6af828ebda
Kyle Harte <36331229+kyleharte@users.noreply.github.com>
2019-05-19 19:11:50 +0100

    Added Examples

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo apt-get update && sudo apt-get install python3 python3-pip && pip3 install speedtest-cli
+sudo apt-get update && sudo apt-get install python3 python3-pip && pip3 install speedtest-cli && pip3 install matplotlib
